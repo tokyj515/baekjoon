17135681998456c446ef3c2d88e4f8784276d4d6
Yujin Kwon <tokyj515@gmail.com>
2022-05-25 11:10:59 +0900

    22.05.25

---
+++ bj11719.cpp
@@ -0,0 +1,27 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> P;
+
+
+
+/*
+int main() {
+    string text;
+    while (getline(cin, text)) {
+        cout << text << endl;
+    }
+}*/
