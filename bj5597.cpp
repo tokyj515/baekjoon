5aa01026e85111488f3b79058a16ff33a972a3a4
Yujin Kwon <tokyj515@gmail.com>
2022-05-26 17:21:50 +0900

    22.05.26

---
+++ bj5597.cpp
@@ -0,0 +1,50 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> P;
+
+
+/*
+int visited[35];
+
+int main() {
+	
+	
+	for (int i = 1; i <= 28; i++) {
+		int num;
+		scanf("%d", &num);
+
+		visited[num] = 1;
+	}
+
+	int cnt = 0;
+
+	for (int i = 1; i <= 30; i++) {
+		if (!visited[i]) {
+			printf("%d\n", i);
+			cnt++;
+
+			if (cnt == 2) return 0;
+			
+		}
+	
+	
+	}
+
+
+
+	return 0;
+}*/
