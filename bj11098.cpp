048556e975eccaada98b6103583d6094973c1fdd
Yujin Kwon <tokyj515@gmail.com>
2022-05-27 12:52:14 +0900

    22.05.27

---
+++ bj11098.cpp
@@ -0,0 +1,66 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> P;
+
+
+
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+
+	while (n--) {
+
+		int p;
+		scanf("%d", &p);
+
+		vector<pair<int, string>> person(p);
+		string ans;
+		int maxVal = 0;
+
+		for (int i = 0; i < p; i++) {
+			int money;
+			string name;
+
+			cin >> money >> name;
+			
+			person.push_back({ money, name });
+
+
+			if (money > maxVal) {
+				maxVal = money;
+				ans = name;
+			}
+			
+		
+		}
+
+		
+		cout << ans << "\n";
+	
+		
+		
+	
+	
+	
+	}
+
+
+
+	return 0;
+}
