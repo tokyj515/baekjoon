7394dae881c0cbff1554b67fe8a3a6a56047b220
Yujin Kwon <tokyj515@gmail.com>
2022-06-02 10:43:26 +0900

    22.06.02

---
+++ bj1003.cpp
@@ -0,0 +1,61 @@
+﻿#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int zero = 0;
+int one = 0;
+
+int fibonacci(int n) {
+    if (n == 0) {
+        //printf("0");
+        zero++;
+        return 0;
+    }
+    else if (n == 1) {
+        //printf("1");
+        one++;
+        return 1;
+    }
+    else {
+        return fibonacci(n‐1) + fibonacci(n‐2);
+    }
+}
+
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+
+    while (t--) {
+        int num;
+
+        fibonacci(num);
+
+        printf("%d %d\n", zero, one);
+
+        zero = 0;
+        one = 0;
+        
+    }
+
+
+
+	return 0;
+}*/
---
+++ bj1712.cpp
@@ -0,0 +1,34 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (b >= c) {
+		printf("-1\n");
+		return 0;
+	}
+
+	printf("%d\n", a / (c - b) + 1);
+
+	return 0;
+}*/
