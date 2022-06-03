8f3cec5db84b2280899d0c67cd930f61493fb557
Yujin Kwon <tokyj515@gmail.com>
2022-06-03 11:15:53 +0900

    22.06.03

---
+++ bj11945.cpp
@@ -0,0 +1,38 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+string num[10];
+
+int main() {
+	int N, M;
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+		reverse(num[i].begin(), num[i].end());
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << num[i] << "\n";
+	}
+
+	return 0;
+}*/
