5ff8271013773c24265022fe939e756943175da0
Yujin Kwon <tokyj515@gmail.com>
2022-06-05 11:40:58 +0900

    22.06.05

---
+++ bj16435.cpp
@@ -0,0 +1,45 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int height[10001];
+
+int main() {
+	int N, L;
+	scanf("%d %d", &N, &L);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &height[i]);
+	}
+
+	sort(height, height + N);
+
+	for (int i = 0; i < N; i++) {
+		if (height[i] <= L) {
+			L++;
+		}
+	
+	}
+
+	printf("%d\n", L);
+
+
+	return 0;
+}*/
