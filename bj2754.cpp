29ac8b336e92e691aa1a52eb32f793ff214cfbfb
Yujin Kwon <tokyj515@gmail.com>
2022-06-07 19:53:58 +0900

    22.06.07

---
+++ bj2754.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+
+/*
+map<string, string> m = {
+	{"A+", "4.3"}, {"A0", "4.0"}, {"A-", "3.7"}
+	,{"B+", "3.3"},{"B0", "3.0"},{"B-", "2.7"}
+	,{"C+", "2.3"},{"C0", "2.0"},{"C-", "1.7"}
+	,{"D+", "1.3"},{"D0", "1.0"},{"D-", "0.7"}
+	,{"F", "0.0"}
+};
+
+int main() {
+
+
+	string s;
+	cin >> s;
+
+	cout << m[s] << "\n";
+
+	return 0;
+}
+*/
+
