447ed1087fef246222c8230954fbd741aaacad47
Yujin Kwon <tokyj515@gmail.com>
2022-06-09 12:55:15 +0900

    22.06.09

---
+++ bj23037.cpp
@@ -0,0 +1,40 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+
+int main() {
+	string s;
+	cin >> s;
+
+	ll sum = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		ll temp = 0;
+		temp = pow(s[i] - '0', 5);
+		
+		sum += temp;
+	}
+
+	cout << sum << "\n";
+
+	return 0;
+}*/
