44bca71642f069fcbbd70b8b2e0c8d4a535e3754
Yujin Kwon <tokyj515@gmail.com>
2022-06-11 17:44:20 +0900

    22.06.11

---
+++ bj2738.cpp
@@ -0,0 +1,52 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int num[101][101];
+
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%d", &num[i][j]);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int temp = num[i][j];
+			scanf("%d", &num[i][j]);
+			num[i][j] += temp;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d ", num[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}*/
