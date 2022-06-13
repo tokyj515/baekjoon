358b0abaa09e320cae70dd99cb848595fac094f6
Yujin Kwon <tokyj515@gmail.com>
2022-06-13 15:11:32 +0900

    22.06.13

---
+++ bj9501.cpp
@@ -0,0 +1,47 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int t;
+	scanf("%d", &t);
+
+
+
+
+	while (t--) {
+		int cnt = 0;
+		int N, D;
+		scanf("%d %d", &N, &D);
+	
+		while (N--) {
+			int v, f, c;
+			scanf("%d %d %d", &v, &f, &c);
+
+			if (v * f / c >= D)
+				cnt++;
+		}
+
+		printf("%d\n", cnt);
+	}
+
+
+	return 0;
+}*/
