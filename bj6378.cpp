661acd7e008bb5105af1c74ae0fa5359b4b8ebef
Yujin Kwon <tokyj515@gmail.com>
2022-06-14 20:12:35 +0900

    22.06.14

---
+++ bj6378.cpp
@@ -0,0 +1,51 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int main() {
+
+	while (true) {
+		string n;
+		cin >> n;
+
+		if (n == "0") {
+			break;
+		}
+
+		
+		int total = 0;
+		for (int i = 0; i < n.size(); i++) {
+			total += n[i] - '0';
+		}
+
+
+		while (total >= 10) {
+			total = total / 10 + total % 10;
+		}
+	
+		cout << total << "\n";
+	}
+
+
+
+
+	return 0;
+}*/
