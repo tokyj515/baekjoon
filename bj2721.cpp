32a1a6a597283e03bd6ed8b1abd7013d917ada06
Yujin Kwon <tokyj515@gmail.com>
2022-06-15 14:02:15 +0900

    22.06.15

---
+++ bj2721.cpp
@@ -0,0 +1,62 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int T(int n) {
+    return n * (n + 1) / 2;
+}
+
+
+int main(void) {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    int t;
+    cin >> t;
+
+
+    while(t--) {
+
+
+
+        int n;
+        cin >> n;
+
+        int result = 0;
+
+
+        for (int i = 1; i <= n; i++) {
+
+            result += i * T(i + 1);
+
+        }
+
+        cout << result << "\n";
+
+
+    }
+
+    return 0;
+}
+*/
