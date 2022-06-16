dc946748106e64dc181973c0636789b9faf46336
Yujin Kwon <tokyj515@gmail.com>
2022-06-16 11:10:00 +0900

    22.06.16

---
+++ bj12780.cpp
@@ -0,0 +1,45 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	string h, n;
+	cin >> h >> n;
+
+	int cnt = 0;
+
+
+	for (int i = 0; i < h.size() - n.size() + 1; i++) {
+		bool flag = true;
+		for (int j = 0; j < n.size(); j++) {
+			if (h[i + j] != n[j]) {
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag) cnt++;
+	}
+
+	cout << cnt << "\n";
+
+
+	return 0;
+}*/
