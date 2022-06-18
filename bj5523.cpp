ce7d2d2d246a19c1905e278e80e3b2210ab9c5f4
Yujin Kwon <tokyj515@gmail.com>
2022-06-18 20:03:02 +0900

    22.06.18

---
+++ bj5523.cpp
@@ -0,0 +1,38 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int t;
+	cin >> t;
+
+	int aWin = 0, bWin = 0;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		if (a > b) aWin++;
+		else if (a < b) bWin++;
+	}
+
+
+	cout << aWin << " " << bWin << "\n";
+
+	return 0;
+}*/
