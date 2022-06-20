58cdc09aa6c550cb7ada983ab72feed35a71f343
Yujin Kwon <tokyj515@gmail.com>
2022-06-20 11:14:55 +0900

    22.06.20

--- bj10757.cpp
+++ bj10757.cpp
@@ -8,72 +8,59 @@ using namespace std;
 #include <stack>
 #include <queue>
 #include <deque>
+#include <functional>
 #include <cmath>
+#include <map>
+#include <set>
 #define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
 
 /*
-
-string A, B;
-
-void zero(string a, string b) {
-	long long gap;
-	
-	if (a.size() > b.size()) {
-		gap = a.size() - b.size();
-
-		while (gap--) {
-			b = "0" + b;
-		}
-		A = a;
-		B = b;
-		printf("%s %s\n", A.c_str(), b.c_str());
-	}
-	else if (a.size() < b.size()) {
-		gap = b.size() - a.size();
-
-		while (gap--) {
-			a = "0" + a;
-		}
-		A = a;
-		B = b;
-		printf("%s %s\n", A.c_str(), b.c_str());
-	}
-	else {//a.size() == b.size()
-		A = a;
-		B = b;
-		printf("%s %s\n", A.c_str(), b.c_str());
-	}
-
-
-}
-string add(string a, string b) {
-	string ans = "";
-	long long len = a.size() - 1;
-	int carry = 1;
-
-	for (int i = len; i >= 0; i--) {
-		
-	
-	
-	
-	}
-
-
-
-
-
-	return ans;
-}
+int N, sum;
+int num1[10001], num2[10001];
+string s1, s2, temp;
+vector<int> ans;
 
 int main() {
-	cin >> A >> B;
+	cin >> s1 >> s2;
 
-	zero(A, B);
+	if (s1.size() < s2.size()) {
+		temp = s1;
+		s1 = s2;
+		s2 = temp;
+	}
 
-	string ans = add(A, B);
 
-	printf("%s", ans.c_str());
+	for (int i = 0; i < s1.size(); i++)
+		num1[i + 1] = s1[i] - '0';
 
+	for (int i = 0; i < s2.size(); i++)
+		num2[i + 1 + (s1.size() - s2.size())] = s2[i] - '0';
+
+
+
+	for (int i = s1.size(); i > 0; i--)	{
+		sum = num1[i] + num2[i];
+
+		if (sum >= 10) {
+			num1[i - 1]++;
+			sum -= 10;
+		}
+
+		ans.push_back(sum);
+	}
+
+
+	if (num1[0] != 0) 
+		cout << 1;
+
+
+	for (int i = ans.size() - 1; i >= 0; i--) { 
+		cout << ans[i];
+	}
 
 	return 0;
 }*/
