2255d93fbd5b9f158772ce957cfb4d7c9f473cd3
Yujin Kwon <tokyj515@gmail.com>
2022-06-21 12:25:12 +0900

    22.06.21

---
+++ bj4892.cpp
@@ -0,0 +1,57 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int idx = 0;
+
+	while (true) {
+
+		int n1, n2, n3, n4;
+		int N;
+		cin >> N;
+
+		if (N == 0) break;
+
+		idx++;
+		cout << idx << ". ";
+
+
+		n1 = N * 3;
+
+		if (n1 % 2 == 0) {
+			cout << "even ";
+			n2 = (n1 + 1) / 2;
+		}
+		else {
+			cout << "odd ";
+			n2 = n1 / 2;
+		}
+
+		n3 = 3 * n2;
+		n4 = n3 / 9;
+
+		cout << n4 <<  "\n";
+	}
+
+
+
+	return 0;
+}*/
