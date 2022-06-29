3d85039619343ac10a1987fb77ce8a64fce4e117
Yujin Kwon <tokyj515@gmail.com>
2022-06-29 11:04:55 +0900

    22.06.29

---
+++ bj25206.cpp
@@ -0,0 +1,63 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+map<string, double> m;
+
+
+int main() {
+	m["A+"] = 4.5;
+	m["A0"] = 4.0;
+	m["B+"] = 3.5;
+	m["B0"] = 3.0;
+	m["C+"] = 2.5;
+	m["C0"] = 2.0;
+	m["D+"] = 1.5;
+	m["D0"] = 1.0;
+	m["F"] = 0.0;
+	
+	
+	double sum = 0.0;
+	double totalTime = 0.0;
+	int t = 20;
+	int cnt = 20;
+
+
+	while (t--) {
+
+		string name, grade;
+		double time;
+
+		cin >> name >> time >> grade;
+		totalTime += time;
+
+		if (grade == "P") {
+			totalTime -= time;
+		}
+		else {
+			sum += time * m[grade];
+		}
+	}
+
+	//cout << sum / totalTime  << "\n";
+
+	printf("%.6f\n", sum / totalTime);
+
+	return 0;
+}*/
