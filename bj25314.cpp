b0f6d203da04ce4966dd402b0fa45d05bf1b0f78
Yujin Kwon <tokyj515@gmail.com>
2022-07-04 11:45:03 +0900

    22.07.04

---
+++ bj25314.cpp
@@ -0,0 +1,35 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int n;
+	cin >> n;
+
+	int cnt = n / 4;
+
+	while (cnt--) {
+		cout << "long ";
+	}
+
+	cout << "int";
+
+	return 0;
+}*/
