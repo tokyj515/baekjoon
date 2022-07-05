85b1c71ea518a517e90ee08270ce0c8227f3b1ed
Yujin Kwon <tokyj515@gmail.com>
2022-07-05 10:12:55 +0900

    22.07.05

---
+++ bj25304.cpp
@@ -0,0 +1,43 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+	ll total;
+	int t;
+
+	cin >> total >> t;
+
+	while (t--) {
+		int a, b; //가격, 개수
+		cin >> a >> b;
+
+		total -= a * b;
+	}
+
+	if (total == 0) {
+		cout << "Yes" << "\n";
+	}
+	else {
+		cout << "No" << "\n";
+	}
+
+	return 0;
+}
+*/
