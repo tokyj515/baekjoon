eb8aef511023096cc8f05675f4728069d784ffea
Yujin Kwon <tokyj515@gmail.com>
2022-07-06 11:11:11 +0900

    22.07.06

---
+++ bj25305.cpp
@@ -0,0 +1,45 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+bool compare(int a, int b) {
+	return a > b;
+}
+
+
+int main() {
+	vector<int> num;
+
+	int n, prize;
+	cin >> n >> prize;
+
+	while (n--) {
+		int score;
+		cin >> score;
+		num.push_back(score);
+	}
+
+	sort(num.begin(), num.end(), compare);
+
+	cout << num[prize - 1];
+
+
+
+	return 0;
+}*/
