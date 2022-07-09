3dc85dfa6fb349a4f64d743053c6ef7adaeb4b71
Yujin Kwon <tokyj515@gmail.com>
2022-07-09 20:14:50 +0900

    22.07.09

---
+++ bj2935.cpp
@@ -0,0 +1,37 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int a, b;
+	char op;
+	cin >> a >> op >> b;
+
+	if (op == '+') {
+		cout << a + b;
+	}
+	else if (op == '*') {
+		cout << a * b;
+	}
+
+
+
+	return 0;
+}*/
---
+++ bj9610.cpp
@@ -0,0 +1,59 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int t;
+	cin >> t;
+
+	int q1 = 0, q2 = 0, q3 = 0, q4 = 0, axis = 0;
+
+	while (t--) {
+		int x, y;
+		cin >> x >> y;
+
+		if (x > 0 && y > 0) {
+			q1++;
+		}
+		else if (x < 0 && y>0) {
+			q2++;
+		}
+		else if (x < 0 && y < 0) {
+			q3++;
+		}
+		else if (x > 0 && y < 0) {
+			q4++;
+		}
+		else {
+			axis++;
+		}
+	
+	}
+
+
+	printf("Q1: %d\n", q1);
+	printf("Q2: %d\n", q2);
+	printf("Q3: %d\n", q3);
+	printf("Q4: %d\n", q4);
+	printf("AXIS: %d\n", axis);
+
+
+	return 0;
+}*/
