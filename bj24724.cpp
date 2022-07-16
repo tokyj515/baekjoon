e3acb1b0c797e5b5de1bf9c0795a246cde24a785
Yujin Kwon <tokyj515@gmail.com>
2022-07-16 20:17:46 +0900

    22.07.16

---
+++ bj24724.cpp
@@ -0,0 +1,45 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int main() {
+	int t;
+	cin >> t;
+
+	for (int i = 1; i <= t; i++) {
+		int n;
+		int A, B;
+		cin >> n;
+		cin >> A >> B;
+
+		while (n--) {
+			int x, y;
+			cin >> x >> y;
+
+		}
+	
+
+		cout << "Material Management " << i << "\n";
+		cout << "Classification ---- End!" << "\n";
+	}
+
+	return 0;
+}*/
