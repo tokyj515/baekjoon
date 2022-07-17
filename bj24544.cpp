ce7885f4cddd063bd17b6a7217b691badc28bc86
Yujin Kwon <tokyj515@gmail.com>
2022-07-17 12:16:56 +0900

    22.07.17

---
+++ bj24544.cpp
@@ -0,0 +1,52 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+
+/*
+int interest[1000001];
+int registered[1000001];
+
+int main() {
+	int t;
+	cin >> t;
+
+	int totalSum = 0;
+	int Sum = 0;
+
+
+	for (int i = 0; i < t; i++) {
+		cin >> interest[i];
+		totalSum += interest[i];
+	}
+
+
+	for (int i = 0; i < t; i++) {
+		cin >> registered[i];
+
+		if (registered[i] == 0) {
+			Sum += interest[i];
+		}
+	}
+
+		
+	cout << totalSum << "\n" << Sum << "\n";
+
+	return 0;
+}*/
