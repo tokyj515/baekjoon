a65a80b211974a947b36c38f3f1f5bc54de99ebf
Yujin Kwon <tokyj515@gmail.com>
2022-07-20 17:52:09 +0900

    22.07.20

---
+++ bj7567.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+/*
+int main() {
+    string k;
+    int sum = 10;
+    cin >> k;
+    for (int i = 0; i < k.size(); i++)
+    {
+        if ((k[i] == ')' && k[i + 1] == '(')
+            || (k[i] == '(' && k[i + 1] == ')')
+            && i + 1 < k.size())
+        {
+            sum += 10;
+        }
+        else if ((k[i] == ')' && k[i + 1] == ')')
+            || (k[i] == '(' && k[i + 1] == '(')
+            && i + 1 < k.size())
+        {
+            sum += 5;
+        }
+    }
+    cout << sum << '\n';
+}*/
