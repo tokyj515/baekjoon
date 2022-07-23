c6337bb2a5ef6feef4dd357527283c5f4eeaa08c
Yujin Kwon <tokyj515@gmail.com>
2022-07-23 17:44:31 +0900

    22.07.23

---
+++ bj3053.cpp
@@ -0,0 +1,34 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	double num;
+	cin >> num;
+
+	double PI = 3.1415926535897932;
+
+	cout << fixed;
+	cout.precision(6);
+	cout << PI * num * num << "\n";
+	cout << 2 * num * num << "\n";
+
+	return 0;
+}*/
