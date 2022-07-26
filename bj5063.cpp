1e0ba31fa3591e09690374440c89eaad20125975
Yujin Kwon <tokyj515@gmail.com>
2022-07-26 14:06:27 +0900

    22.07.26

---
+++ bj5063.cpp
@@ -0,0 +1,54 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+
+/*
+int main() {
+	int t;
+	cin >> t;
+
+
+	while (t--) {
+		//r은 광고를 하지 않았을 때 수익, e는 광고를 했을 때의 수익, c는 광고 비용
+		ll r, e, c;
+		cin >> r >> e >> c;
+
+		if (r < e - c) {
+			cout << "advertise" << "\n";
+				
+		}
+		else if (r == e - c) {
+			cout << "does not matter" << "\n";
+		}
+		else {
+			cout << "do not advertise" << "\n";
+		}
+
+		
+		
+	}
+
+
+
+
+	return 0;
+}
+
+*/
