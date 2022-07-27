7839cf90eac57966ad7d0be6b90dd05423b1ef39
Yujin Kwon <tokyj515@gmail.com>
2022-07-27 16:56:38 +0900

    22.07.27

---
+++ bj10214.cpp
@@ -0,0 +1,51 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int yonsei = 0, korea = 0;
+		int y, k;
+
+		for (int i = 0; i < 9; i++) {
+			cin >> y >> k;
+			yonsei += y;
+			korea += k;
+		}
+	
+		if (yonsei > korea) {
+			cout << "Yonsei" << "\n";
+		}
+		else if (yonsei < korea) {
+			cout << "Korea" << "\n";
+		}
+		else {
+			cout << "Draw" << "\n";
+		}
+	
+	}
+
+
+	return 0;
+}*/
