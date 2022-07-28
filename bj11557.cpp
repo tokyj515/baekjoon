95c3363038de5067f5d30ed3c7ca8359e341a80b
Yujin Kwon <tokyj515@gmail.com>
2022-07-28 10:28:37 +0900

    22.07.28

---
+++ bj11557.cpp
@@ -0,0 +1,59 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+vector<pair<string, int>> v;
+
+int main() {
+	int t;
+	cin >> t;
+
+
+	while (t--) {
+		v.clear();
+		int maxVal = 0;
+		int n;
+		cin >> n;
+
+		for (int i = 0; i < n; i++) {
+			string s;
+			int d;
+
+			cin >> s >> d;
+			v.push_back({s, d});
+
+			if (d > maxVal) {
+				maxVal = d;
+			}
+		
+		}
+
+		for (int i = 0; i < v.size(); i++) {
+			if (v[i].second == maxVal) {
+				cout << v[i].first << "\n";
+				
+			}
+		}
+
+	
+	}
+
+
+	return 0;
+}*/
