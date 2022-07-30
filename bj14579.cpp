a0f690517332d0e89cc27d3a47d8891ab07d764b
Yujin Kwon <tokyj515@gmail.com>
2022-07-30 22:14:44 +0900

    22.07.30

---
+++ bj14579.cpp
@@ -0,0 +1,48 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+
+/*
+ll hap(int s, int e) {
+	ll sum = 0;
+
+	for (int i = s; i <= e; i++) {
+		sum += i;
+	}
+
+	return sum;
+}
+
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	ll ans = 1;
+
+
+	for (int i = a; i <= b; i++) {
+		ans *= hap(1, i);
+		ans %= 14579;
+	}
+
+	cout << ans << "\n";
+	return 0;
+}*/
