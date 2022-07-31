01b486dcc0bd0ed9b47925d79c39a9b69d599b74
Yujin Kwon <tokyj515@gmail.com>
2022-07-31 13:32:43 +0900

    22.07.31

---
+++ bj15596.cpp
@@ -0,0 +1,30 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+ll sum(vector<int>& a) {
+	ll res = 0;
+
+	for (int i = 0; i < a.size(); i++) {
+		res += a[i];
+	}
+
+	return res;
+}*/
