a988fea4fbd6a53faa988e3255b23f459e334550
Yujin Kwon <tokyj515@gmail.com>
2022-08-08 18:39:18 +0900

    22.08.08

---
+++ bj5532.cpp
@@ -0,0 +1,53 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int L, a, b, c, d;
+int korean, math;
+
+int main() {
+	cin >> L >> a >> b >> c >> d;
+
+	if (a % c == 0) {
+		korean = a / c;
+	}
+	else {
+		korean = a / c;
+		korean++;
+	}
+
+
+
+	if (b % d == 0) {
+		math = b / d;
+	}
+	else {
+		math = b / d;
+		math++;
+	}
+
+
+	cout << L - max(korean, math) << "\n";
+
+
+
+
+	return 0;
+}*/
