ea5b67db56a735deedfe8193a8b73b2730ba6532
Yujin Kwon <tokyj515@gmail.com>
2022-08-11 20:11:43 +0900

    22.08.11

---
+++ bj1453.cpp
@@ -0,0 +1,47 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int seat[101];
+int cnt = 0;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int num;
+		cin >> num;
+		
+		if (seat[num] == 0) {
+			seat[num] = 1;
+		}
+		else {
+			cnt++;
+		}
+	
+	}
+
+	cout << cnt << "\n";
+
+
+
+	return 0;
+}*/
