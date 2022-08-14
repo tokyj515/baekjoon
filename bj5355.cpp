ea09c8335731b3848e708e67a80e48a9310f1338
Yujin Kwon <tokyj515@gmail.com>
2022-08-14 19:18:16 +0900

    22.08.14

---
+++ bj5355.cpp
@@ -0,0 +1,54 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+        char ch;
+		double num;
+		cin >> num;
+
+        while (1) {
+            cin.get(ch);
+            if (ch == '\n') {
+                break;
+            }
+            else if (ch == '@') {
+                num *= 3;
+            }
+            else if (ch == '%') {
+                num += 5;
+            }
+            else if (ch == '#') {
+                num -= 7;
+            }
+        }
+
+
+        cout << fixed;
+        cout.precision(2);
+        cout << num << "\n";
+	}
+
+    return 0;
+}*/
