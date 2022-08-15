33402d608f0e38997c9f97f5f16f989d1df46894
Yujin Kwon <tokyj515@gmail.com>
2022-08-15 22:39:19 +0900

    22.08.15

---
+++ bj15969.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int maxVal = -987654321, minVal = 987654321;
+
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int num;
+		cin >> num;
+
+		if (num > maxVal) maxVal = num;
+
+		if (num < minVal) minVal = num;
+	}
+
+
+	cout << maxVal - minVal << "\n";
+
+	return 0;
+}*/
