cba9d65241474c73555249787a0263407cd536e3
Yujin Kwon <tokyj515@gmail.com>
2022-08-16 10:52:07 +0900

    22.08.16

---
+++ bj3047.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+vector<int> v(3);
+string s;
+
+
+int main() {
+
+	for (int i = 0; i < 3; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+
+	cin >> s;
+
+	for (int i = 0; i < 3; i++) {
+		if (s[i] == 'A') cout << v[0] << " ";
+		if (s[i] == 'B') cout << v[1] << " ";
+		if (s[i] == 'C') cout << v[2] << " ";
+	}
+
+
+	return 0;
+}*/
