60854a6803f5edab09dc5d992fac2ba899859496
Yujin Kwon <tokyj515@gmail.com>
2022-08-18 20:44:44 +0900

    22.08.18

---
+++ bj10768.cpp
@@ -0,0 +1,32 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int m, d;
+	cin >> m >> d;
+
+	if (m < 2 || (m == 2 && d < 18)) cout << "Before" << "\n";
+	else if (m > 2 || (m == 2 && d > 18)) cout << "After" << "\n";
+	else if(m==2 && d==18) cout << "Special" << "\n";
+
+
+	return 0;
+}*/
