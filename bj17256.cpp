84a71325db5e482661d02f28df01c11c42b2e713
Yujin Kwon <tokyj515@gmail.com>
2022-08-19 15:48:24 +0900

    22.08.19

---
+++ bj17256.cpp
@@ -0,0 +1,32 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int ax, ay, az;
+	int cx, cy, cz;
+
+	cin >> ax >> ay >> az;
+	cin >> cx >> cy >> cz;
+
+	cout << cx - az << " " << cy / ay << " " << cz - ax;
+
+	return 0;
+}*/
