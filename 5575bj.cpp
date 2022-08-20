32995eb80bec5491cc3519691125ac3df8e434ab
Yujin Kwon <tokyj515@gmail.com>
2022-08-20 14:56:52 +0900

    22.08.20

---
+++ 5575bj.cpp
@@ -0,0 +1,58 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int t = 3;
+	int sh, sm, ss, eh, em, es;
+	int h, m, s;
+
+
+	while (t--) {
+		cin >> sh >> sm >> ss >> eh >> em >> es;
+
+		if (es - ss < 0) {
+			em--;
+			es += 60;
+			s = es - ss;
+		}
+		else {
+			s = es - ss;
+		}
+
+		if (em - sm < 0) {
+			eh--;
+			em += 60;
+			m = em - sm;
+		}
+		else {
+			m = em - sm;
+		}
+
+
+		h = eh - sh;
+
+		cout << h <<" " << m << " " << s << "\n";
+	
+	}
+
+	return 0;
+}
+*/
