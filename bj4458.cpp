06112a09865880a3869614efba8df165b2bc4b01
Yujin Kwon <tokyj515@gmail.com>
2022-08-21 11:39:52 +0900

    22.08.21

---
+++ bj4458.cpp
@@ -0,0 +1,43 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int main() {
+	int t;
+	cin >> t;
+	cin.ignore();
+
+
+	while (t--) {
+		string s;
+		getline(cin, s);
+
+		if ('a' <= s[0] && s[0] <= 'z') {
+			s[0] -= 32;
+		}
+
+		cout << s << "\n";
+	
+	}
+
+
+	return 0;
+}*/
