a9f4dd40d1abee40391a611058a8d95c76e200f5
Yujin Kwon <tokyj515@gmail.com>
2022-08-22 18:25:18 +0900

    22.08.22

---
+++ bj11948.cpp
@@ -0,0 +1,72 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+vector<int> v;
+
+
+int main() {
+	int total = 0;
+	int minVal = 100;
+
+
+	int t = 4;
+	while (t--) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+
+		if (num < minVal)
+			minVal = num;
+	}
+
+
+	for (int i = 0; i < 4; i++) {
+		if (v[i] == minVal) {
+			v[i] = 0;
+			break;
+		}
+	}
+
+
+	for (int i = 0; i < 4; i++) {
+		total += v[i];
+	}
+
+	v.clear();
+	
+	t = 2;
+	while (t--) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+
+	if (v[0] > v[1]) {
+		total += v[0];
+	}
+	else {
+		total += v[1];
+	}
+
+
+	cout << total << "\n";
+
+	return 0;
+}*/
