4c2e48b67444e818eb59192a107be6dbaafa179b
Yujin Kwon <tokyj515@gmail.com>
2022-08-23 19:32:59 +0900

    22.08.23

---
+++ bj2857.cpp
@@ -0,0 +1,48 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+/*
+
+string ans = "";
+
+int main() {
+
+	for (int i = 1; i <= 5; i++) {
+		string s;
+		cin >> s;
+
+		if (s.find("FBI") == string::npos)
+			continue;
+		else {
+			ans += to_string(i);
+		}
+	}
+
+
+
+	if (ans.size() == 0) 
+		cout << "HE GOT AWAY!\n";
+	else {
+		for (int i = 0; i < ans.size(); i++) {
+			cout << ans[i] << " ";
+		}
+	}
+	
+
+	return 0;
+}*/
