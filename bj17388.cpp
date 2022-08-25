cb2c39d109fe2d7b733dee45667e9e5ed6058e15
Yujin Kwon <tokyj515@gmail.com>
2022-08-25 11:53:57 +0900

    22.08.25

---
+++ bj17388.cpp
@@ -0,0 +1,40 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int s, k, h;
+	cin >> s >> k >> h;
+
+	if (s + k + h >= 100)
+		cout << "OK" << "\n";
+	else {
+		int minVal = min({ s, k, h });
+
+		if (minVal == s)
+			cout << "Soongsil" << "\n";
+		else if (minVal == k) 
+			cout << "Korea" << "\n";
+		else 
+			cout << "Hanyang" << "\n";
+	}
+
+	return 0;
+}*/
