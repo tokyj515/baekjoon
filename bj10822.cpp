ee3f1fbd44357f91ed4dc29e1865b92a0ee7129e
Yujin Kwon <tokyj515@gmail.com>
2022-08-27 14:46:35 +0900

    22.08.27

---
+++ bj10822.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+	string s;
+	cin >> s;
+
+	int sum = 0;
+	string temp = "";
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ',') {
+			temp += s[i];
+		}
+		else {
+			sum += stoi(temp);
+			temp = "";
+		}
+	
+	}
+
+	cout << sum + stoi(temp) << "\n";
+
+
+	return 0;
+}*/
