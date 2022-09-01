b0dbb7e6984c7e55ef41cf96f365897aadcc715f
Yujin Kwon <tokyj515@gmail.com>
2022-09-01 18:40:39 +0900

    22.09.01

---
+++ bj2566.cpp
@@ -0,0 +1,52 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int num[10][10];
+int maxVal;
+int row, col;
+
+int main() {
+
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			cin >> num[i][j];
+
+			if (maxVal < num[i][j]) {
+				maxVal = num[i][j];
+				row = i;
+				col = j;
+			}
+		}
+	}
+
+
+	if (maxVal == 0) {
+		cout << 0 << "\n" << 1 << " " << 1;
+	}
+	else {
+		cout << maxVal << "\n" << row << " " << col;
+	}
+
+
+
+
+	return 0;
+}*/
