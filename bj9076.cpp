bdb55d0d70d828e13fd767c9dc739154a17ff5f4
Yujin Kwon <tokyj515@gmail.com>
2022-09-02 21:05:43 +0900

    22.09.02

---
+++ bj9076.cpp
@@ -0,0 +1,48 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int t;
+vector<int> v(5);
+int num;
+
+int main() {
+	cin >> t;
+
+	while (t--) {
+		v.clear();
+
+		for (int i = 0; i < 5; i++) {
+			cin >> num;
+			v.push_back(num);
+		}
+		
+		sort(v.begin(), v.end());
+
+		if (v[3] - v[1] >= 4) {
+			cout << "KIN" << "\n";
+		}
+		else {
+			cout << v[1] + v[2] + v[3] << "\n";
+		}
+	}
+
+	return 0;
+}*/
