c0424d66096bd9f81ef42acbc63fc6711eccaacd
Yujin Kwon <tokyj515@gmail.com>
2022-09-03 18:19:31 +0900

    22.09.03

---
+++ bj13866.cpp
@@ -0,0 +1,30 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	cout << abs((a + d) - (b + c)) << "\n";
+
+
+	return 0;
+}*/
