e7fcf3ca519dba37e028043211ee230457391bbc
Yujin Kwon <tokyj515@gmail.com>
2022-09-06 12:14:52 +0900

    22.09.06

---
+++ bj10179.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		double money;
+		cin >> money;
+		//cin.ignore();
+
+		printf("$%.2f\n", money * 0.8);	
+	}
+
+
+	return 0;
+}*/
