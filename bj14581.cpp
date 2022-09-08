4c159338d7dca8f838ff397010c189faca173b57
Yujin Kwon <tokyj515@gmail.com>
2022-09-08 19:08:54 +0900

    22.09.08

---
+++ bj14581.cpp
@@ -0,0 +1,32 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int main() {
+	string s;
+	cin >> s;
+
+	cout << ":fan::fan::fan:" << "\n";
+	cout << ":fan::" + s + "::fan:" << "\n";
+	cout << ":fan::fan::fan:" << "\n";
+
+	return 0;
+}*/
---
+++ bj18127.cpp
@@ -0,0 +1,37 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int n, t;
+	cin >> n >> t;
+
+	int sum = 0;
+	int num = 1;
+
+	while(t-- > -1) {
+		sum += num;
+		num += n - 2;
+	}
+
+	cout << sum << "\n";
+
+	return 0;
+}*/
