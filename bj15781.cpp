d314f80f67a598f53e3dceb3583e3e2b2d0b6f17
Yujin Kwon <tokyj515@gmail.com>
2022-09-16 10:32:28 +0900

    22.09.16

---
+++ bj15781.cpp
@@ -0,0 +1,45 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+
+	vector<int> helmet(n);
+	vector<int> vest(m);
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		helmet.push_back(num);
+	}
+
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		vest.push_back(num);
+	}
+
+	cout << *max_element(helmet.begin(), helmet.end()) + *max_element(vest.begin(), vest.end()) << "\n";
+
+	return 0;
+}*/
