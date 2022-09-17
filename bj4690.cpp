2f8a0e190cfb1c4431fc28646bdc5d9090b6800f
Yujin Kwon <tokyj515@gmail.com>
2022-09-17 14:12:22 +0900

    22.09.17

---
+++ bj4690.cpp
@@ -0,0 +1,50 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+
+	for (int a = 6; a <= 100; a++) {
+		for (int b = 2; b < a; b++) {
+			for (int c = b + 1; c < a; c++) {
+				for (int d = c + 1; d < a; d++) {
+
+					if (a * a * a == b * b * b + c * c * c + d * d * d) {
+						printf("Cube = %d, Triple = (%d,%d,%d)\n", a, b, c, d);
+
+					}
+
+				}
+			}
+		}
+	}
+
+
+	return 0;
+}*/
+
+
+/*
+
+    for (int a = 4; a <= 100; a++) {
+        for (int b = 2; b <= a; b++) {
+            for (int c = b; c <= a; c++) {
+                for (int d = c; d <= a; d++) {
+*/
