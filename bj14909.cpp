b37f73b477bca2605b75fd6429b8f5a872c1c74f
Yujin Kwon <tokyj515@gmail.com>
2022-09-25 08:13:09 +0900

    22.09.25

---
+++ bj14909.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	int cnt = 0;
+	while (true) {
+		int num;
+		cin >> num;
+
+		if (cin.eof() == 1) { break; }
+		if (num > 0) { cnt++; }
+	}
+
+	cout << cnt << "\n";
+}*/
