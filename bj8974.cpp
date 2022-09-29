9f7d692552ba0a142f3447b595df9bcfe5b47d1c
Yujin Kwon <tokyj515@gmail.com>
2022-09-29 15:29:59 +0900

    22.09.29

---
+++ bj8974.cpp
@@ -0,0 +1,46 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+
+    vector<int> vec;
+
+
+    for (int i = 1; i <= 1000; i++) {
+        for (int j = 1; j <= i; j++) {
+           
+            vec.push_back(i);
+        }
+    }
+
+    int sum = 0;
+
+    for (int i = a - 1; i <= b - 1; i++) {
+        sum += vec[i];
+    }
+
+    cout << sum;
+    return 0;
+}
+*/
