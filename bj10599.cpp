cedbf089d5875bec2af088af44766c68f160d35c
Yujin Kwon <tokyj515@gmail.com>
2022-10-02 14:33:15 +0900

    22.10.02

---
+++ bj10599.cpp
@@ -0,0 +1,40 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+
+int birthStart, birthEnd, deadStart, deadEnd;
+int maxVal, minVal;
+
+
+int main() {
+
+	while (true) {
+		cin >> birthStart >> birthEnd >> deadStart >> deadEnd;
+
+		if (birthStart == 0 && birthEnd == 0 && deadStart == 0 && deadEnd == 0) break;
+
+		minVal = abs(deadStart - birthEnd);  
+		maxVal = abs(deadEnd - birthStart);  
+
+		cout << minVal << " " << maxVal << "\n";
+	}
+
+	return 0;
+}*/
