60109803c45befb4019e8dc03e7a270c31c4e46b
Yujin Kwon <tokyj515@gmail.com>
2022-10-03 19:59:03 +0900

    22.10.03

---
+++ bj17826.cpp
@@ -0,0 +1,50 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+vector<int> score(51);
+int hongikScore;
+
+
+int main() {
+
+	for (int i = 1; i <= 50; i++) {
+		cin >> score[i];
+	}
+	cin >> hongikScore;
+
+	for (int i = 1; i <= 50; i++) {
+		if (hongikScore == score[i]) {
+			
+			if (1 <= i && i <= 5) cout << "A+" << "\n";
+			else if (6 <= i && i <= 15) cout << "A0" << "\n";
+			else if (16 <= i && i <= 30) cout << "B+" << "\n";
+			else if (31 <= i && i <= 35) cout << "B0" << "\n";
+			else if (36 <= i && i <= 45) cout << "C+" << "\n";
+			else if (46 <= i && i <= 48) cout << "C0" << "\n";
+			else if (49 <= i && i <= 50) cout << "F" << "\n";
+		
+			return 0;
+		}
+	}
+
+
+	return 0;
+}*/
