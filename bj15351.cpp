56bda3db06ec0d62c44b9743a4f01b672457ee7a
Yujin Kwon <tokyj515@gmail.com>
2022-10-06 23:34:14 +0900

    22.10.06

---
+++ bj15351.cpp
@@ -0,0 +1,54 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+
+	cin.tie(0);
+	std::ios::sync_with_stdio(false);
+
+	string s;
+	int t;
+	cin >> t;
+
+
+	while (t--) {
+		int sum = 0;
+		
+		getline(cin, s);
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] != ' ') {
+				sum += s[i] - 'A' + 1;
+			}
+		}
+
+
+		if (sum == 100) {
+			cout << "PERFECT LIFE" << "\n";
+		}
+		else {
+			cout << sum << "\n";
+		}
+	
+	}
+
+
+	return 0;
+}*/
