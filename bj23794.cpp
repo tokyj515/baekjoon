5deabb03c6ad2d72b2ef6d7c8da4e2300f6e596c
Yujin Kwon <tokyj515@gmail.com>
2022-10-07 16:01:53 +0900

    22.10.07

---
+++ bj23794.cpp
@@ -0,0 +1,56 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+	int n;
+	cin >> n;
+
+	int N = n + 2;
+
+
+
+	for (int i = 0; i < N; i++) {
+		printf("@");
+	}
+	printf("\n");
+
+
+	for (int i = 0; i < n; i++) {
+		printf("@");
+
+		for (int j = 0; j < n; j++) {
+			printf(" ");
+		}
+		printf("@");
+		printf("\n");
+	}
+
+
+
+	for (int i = 0; i < N; i++) {
+		printf("@");
+	}
+	printf("\n");
+
+
+
+
+	return 0;
+}*/
