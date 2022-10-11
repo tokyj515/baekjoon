c1f73246727ec9868ac47e1782e5c4521d7eeaa1
YuJin Kwon <tokyj515@gmail.com>
2022-10-11 11:47:51 +0900

    22.10.11

---
+++ bj1271.cpp
@@ -0,0 +1,39 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/* //C++은 나누기 구현해야 함
+int main() {
+	int money, people;
+	cin >> money >> people;
+
+
+	cout << money / people << "\n";
+	cout << money % people << "\n";
+
+
+	return 0;
+}*/
+
+
+
+/*
+money, people = map(int, input().split())
+print(money//people)
+print(money%people)
+*/
