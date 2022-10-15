ef1b0da51317666ca2ef63c347d8307bef9156e1
YuJin Kwon <tokyj515@gmail.com>
2022-10-15 15:52:17 +0900

    22.10.15

---
+++ bj9506.cpp
@@ -0,0 +1,69 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	while (true) {
+		vector<int> v;
+		int num;
+		cin >> num;
+
+		if (num == -1) break;
+
+
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				v.push_back(i);
+			}
+		}
+
+
+
+		int sum = 0;
+		for (int i = 0; i < v.size(); i++) {
+			sum += v[i];
+		}
+
+
+
+		if (sum == num) {
+			cout << num << " = ";
+			for (int i = 0; i < v.size(); i++) {
+				cout << v[i];
+				if (i != v.size() - 1) {
+					cout << " + ";
+				}
+				else {
+					cout << " " << "\n";
+				}
+			}
+		
+		}
+		else {
+			cout << num << " is NOT perfect." << "\n";
+		}
+
+
+	
+	}
+
+
+	return 0;
+}*/
