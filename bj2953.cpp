bb97b8029e70aec781504dde36a2a8b00c126f0e
YuJin Kwon <tokyj515@gmail.com>
2022-10-16 10:55:11 +0900

    22.10.16

---
+++ bj2953.cpp
@@ -0,0 +1,49 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+vector<int> v(5);
+int score;
+
+
+int main() {
+
+	int maxVal = 0, idx = 0;
+
+	for (int i = 0; i < 5; i++) {
+		int sum = 0;
+
+		for (int j = 0; j < 4; j++) {
+			cin >> score;
+			sum += score;
+		}
+
+
+		if (sum > maxVal) {
+			maxVal = sum;
+			idx = i + 1;
+		}
+	}
+
+
+	cout << idx << " " << maxVal << "\n";
+
+
+	return 0;
+}*/
