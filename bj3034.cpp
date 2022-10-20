3d0597ab7d29de0c0faf4c4f5fd362a73c5495d4
YuJin Kwon <tokyj515@gmail.com>
2022-10-20 12:29:13 +0900

    22.10.22

---
+++ bj3034.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include <math.h>
+
+using namespace std;
+
+
+/*
+int main() {
+
+	int n, w, h;
+
+	cin >> n >> w >> h;
+
+	int* ary = new int[n];
+
+	for (int i = 0; i < n; i++)
+
+		cin >> ary[i];
+
+
+
+	int max = sqrt(w * w + h * h);
+
+	for (int i = 0; i < n; i++) {
+
+		if (max >= ary[i])
+
+			cout << "DA" << "\n";
+
+		else
+
+			cout << "NE" << "\n";
+
+	}
+
+}*/
+
+
