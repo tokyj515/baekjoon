b94ecb875c53aee31ad192a4162e153b116f6468
YuJin Kwon <tokyj515@gmail.com>
2022-10-21 17:01:06 +0900

    22.10.21

---
+++ bj4504.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int num;
+	cin >> num;
+
+	while (true) {
+		int target;
+		cin >> target;
+
+		if (target == 0) break;
+
+		if (target % num == 0) {
+			cout << target << " is a multiple of " << num << ".\n";
+		}
+		else {
+			cout << target << " is NOT a multiple of " << num << ".\n";
+		}
+	}
+
+
+	return 0;
+}*/
