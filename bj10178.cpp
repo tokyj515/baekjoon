eeeb41f1f35db3901910eccfb9d14759cf30ac1b
YuJin Kwon <tokyj515@gmail.com>
2022-10-23 17:17:28 +0900

    22.10.23

---
+++ bj10178.cpp
@@ -0,0 +1,32 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+	int t, candy, people;
+	cin >> t;
+
+	while (t--) {
+		cin >> candy >> people;
+
+		cout << "You get " << candy / people << " piece(s) and your dad gets " << candy % people << " piece(s).\n";
+	}
+
+	return 0;
+}*/
