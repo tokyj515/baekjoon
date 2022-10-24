6c5c474aa8c308255642206065226b462f46ca12
YuJin Kwon <tokyj515@gmail.com>
2022-10-24 13:01:11 +0900

    22.10.24

---
+++ bj2702.cpp
@@ -0,0 +1,43 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+
+		cout << lcm(a, b) << " " << gcd(a, b) << "\n";
+	}
+
+
+}*/
