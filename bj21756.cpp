2f9cf6f9037e3b5d4250755f63be8803c2ec3e21
YuJin Kwon <tokyj515@gmail.com>
2022-10-25 13:03:47 +0900

    22.10.25

---
+++ bj21756.cpp
@@ -0,0 +1,44 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+vector<int> v1, v2;
+int N;
+
+
+int main() {
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		v1.push_back(i);
+	}
+
+
+	while (v1.size() != 1) {
+		v2.clear();
+		for (int i = 1; i < v1.size(); i += 2) 
+			v2.push_back(v1[i]);
+		v1 = v2;
+	}
+
+	cout << v1.front();
+
+	return 0;
+}*/
--- bj2502.cpp
+++ bj2502.cpp
@@ -12,44 +12,24 @@ using namespace std;
 #include <cmath>
 #define _CRT_SECURE_NO_WARNINGS
 
+
 /*
-int dp[35][2]; 
+int a, b;
+int day, tteok;
+int dp[31];
 
 int main() {
-	int d, k; //몇번째날, 떡개수
 
-	scanf("%d %d", &d, &k);
+	cin >> day >> tteok;
 
-	dp[1][1] = 1;
-	dp[1][2] = 0;
-	dp[2][1] = 0;
-	dp[2][2] = 1;
+	dp[1] = a;
+	dp[2] = b;
 
-
-	for (int i = 3; i <= d; i++) {
-		dp[i][1] = dp[i - 2][1] + dp[i - 1][1];
-		dp[i][2] = dp[i - 2][2] + dp[i - 1][2];
+	for (int i = 3; i < 31; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
 	}
-	
 
-	for (int i = 1; i <= k; i++) {
-		if ((k - i * dp[d][1]) % dp[d][2] == 0) {
-			int j = (k - i * dp[d][1]) / dp[d][2];
-
-			if (i <= j) {
-				printf("%d\n%d\n", i, j);
-				return 0;
-			}
-			else if (k < i * dp[d][1] + j * dp[d][2]) {
-				break;
-			}
-
-
-			
-
-		
-		}
-	}
+	dp[day] = tteok;
 
 
 
