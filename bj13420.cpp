d6cfa155d7eeb97050b865b74a0b3237ab9f3e2c
YuJin Kwon <tokyj515@gmail.com>
2022-10-27 17:01:07 +0900

    22.10.27

---
+++ bj13420.cpp
@@ -0,0 +1,67 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int  t;
+	cin >> t;
+
+	while (t--) {
+		ll a, b, ans;
+		char op, equal;
+		cin >> a >> op >> b >> equal >> ans;
+
+		if (op == '+') {
+			if (a + b == ans) {
+				cout << "correct" << "\n";
+			}
+			else {
+				cout << "wrong answer" << "\n";
+			}
+		}
+		else if (op == '-') {
+			if (a - b == ans) {
+				cout << "correct" << "\n";
+			}
+			else {
+				cout << "wrong answer" << "\n";
+			}
+		}
+		else if (op == '*') {
+			if (a * b == ans) {
+				cout << "correct" << "\n";
+			}
+			else {
+				cout << "wrong answer" << "\n";
+			}
+		}
+		else if (op == '/') {
+			if (a / b == ans) {
+				cout << "correct" << "\n";
+			}
+			else {
+				cout << "wrong answer" << "\n";
+			}
+		}
+	}
+
+
+	return 0;
+}*/
