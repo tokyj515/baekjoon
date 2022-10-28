424bbd510df5aed4cbb8319ded873155729eb705
YuJin Kwon <tokyj515@gmail.com>
2022-10-28 22:27:06 +0900

    22.10.28

---
+++ bj14954.cpp
@@ -0,0 +1,50 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int num;
+vector<int> v;
+
+
+int func(int n) {
+	if (n == 1) return 1;
+
+	while (num) {
+		
+	
+	}
+
+
+}
+
+
+int main() {
+	cin >> num;
+
+	if (func(num) == 1) {
+		cout << "HAPPY" << "\n";
+	}
+	else {
+		cout << "UNHAPPY" << "\n";
+	}
+
+
+	return 0;
+}*/
