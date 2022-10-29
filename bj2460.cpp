b1beb4ac81d2d531286017bd53dcf0064cb40a13
YuJin Kwon <tokyj515@gmail.com>
2022-10-29 23:05:40 +0900

    22.10.29

---
+++ bj2460.cpp
@@ -0,0 +1,40 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	int maxVal = 0;
+	int people = 0;
+	int on, off;
+	int t = 10;
+
+	while (t--) {
+		cin >> off >> on;
+
+		people = people - off + on;
+
+		maxVal = max(people, maxVal);
+	}
+
+	cout << maxVal << "\n";
+
+
+	return 0;
+}*/
