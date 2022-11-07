bd167852633ff9b7939e71d79eb965b4a4d68243
YuJin Kwon <tokyj515@gmail.com>
2022-11-07 20:53:28 +0900

    22.11.07

---
+++ bj1568.cpp
@@ -0,0 +1,51 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int second, birds, idx, time;
+
+
+
+int N, ans, bird;
+
+int main() {
+
+    cin >> N;
+
+    ans = bird = 1;
+    while (true) {
+        if (N - bird >= 0) {
+            N -= bird;
+        }
+        else {
+            bird = 1;
+            N -= bird;
+        }
+
+        if (N == 0) {
+            break;
+        }
+
+        bird++;
+        ans++;
+    }
+
+    cout << ans << "\n";
+}*/
