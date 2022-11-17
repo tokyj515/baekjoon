4545f04228e73a106d21c23f253dea63400878a4
YuJin Kwon <tokyj515@gmail.com>
2022-11-17 11:36:07 +0900

    22.11.17

---
+++ bj10101.cpp
@@ -0,0 +1,43 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if (a + b + c != 180) {
+		cout << "Error" << "\n";
+	}
+	else {
+		if (a == b && b == c) {
+			cout << "Equilateral" << "\n";
+		}
+		else if (a != b && b != c && c != a) {
+			cout << "Scalene" << "\n";
+		}
+		else {
+			cout << "Isosceles" << "\n";
+		}
+	
+	}
+
+
+	return 0;
+}*/
