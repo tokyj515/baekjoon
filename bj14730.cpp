37405fb4c7149338c3abc5e3a639c915d965aff4
YuJin Kwon <tokyj515@gmail.com>
2022-11-19 13:49:23 +0900

    22.11.19

---
+++ bj14730.cpp
@@ -0,0 +1,38 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+vector<PII> v(1001);
+int t;
+ll sum = 0;
+
+int main() {
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> v[i].first >> v[i].second;
+
+		sum += v[i].first * v[i].second;
+	}
+
+	cout << sum;
+
+	return 0;
+}*/
