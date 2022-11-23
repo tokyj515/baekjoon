bbcb3e80caff6a528b4c3fe87ea3ea0896ac8468
YuJin Kwon <tokyj515@naver.com>
2022-11-23 12:37:18 +0900

    22.11.23

---
+++ bj2896.cpp
@@ -0,0 +1,41 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main(void) {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+
+	double A, B, C;
+	double I, J, K;
+	cin >> A >> B >> C;
+	cin >> I >> J >> K;
+
+	double a = A / I;
+	double b = B / J;
+	double c = C / K;
+
+	double min = a < b ? a : b;
+	min = min < c ? min : c;
+
+	cout << fixed << setprecision(6) << A - min * I << ' ' << B - min * J << ' ' << C - min * K;
+	return 0;
+}*/
