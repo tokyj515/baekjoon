fa91117c5f730c529161728abfd44fe015c7afc7
YuJin Kwon <tokyj515@naver.com>
2022-11-24 13:08:50 +0900

    22.11.24

---
+++ bj25494.cpp
@@ -0,0 +1,49 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int a, b, c;
+
+int main() {
+	int t;
+	cin >> t;
+
+
+	while (t--) {
+		int cnt = 0;
+		cin >> a >> b >> c;
+
+
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= b; j++) {
+				for (int k = 1; k <= c; k++) {
+					if (i % j == j % k && j % k == k % i) {
+						cnt++;
+					}
+				}
+			}
+		}
+
+		cout << cnt << "\n";
+	}
+
+
+	return 0;
+}*/
