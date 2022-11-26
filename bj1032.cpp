763f7048562440d8fcf91937fa77dbf607969481
YuJin Kwon <tokyj515@naver.com>
2022-11-26 12:27:06 +0900

    22.11.26

---
+++ bj1032.cpp
@@ -0,0 +1,56 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int main() {
+	int t;
+	string ans = "";
+
+	cin >> t;
+
+	for (int k = 1; k <= t; k++) {
+		string s;
+		cin >> s;
+
+		if (k == 1) {
+			ans = s;
+			if (t == 1) break;
+			else continue;
+		}
+	
+		
+		for (int i = 0; i < ans.size(); i++) {
+			if (ans[i] != s[i]) {
+				ans[i] = '?';
+			}
+		}
+
+
+	
+	}
+
+
+
+	cout << ans << "\n";
+
+	return 0;
+}*/
