1003b133840d1411e5508157f1920c166d6f53ac
YuJin Kwon <tokyj515@naver.com>
2022-12-01 21:41:16 +0900

    22.12.01

---
+++ bj1159.cpp
@@ -0,0 +1,55 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int arr[26];
+string ans = "";
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		string s;
+		cin >> s;
+
+		arr[s[0] - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (arr[i] >= 5) {
+			ans += (char)(i + 'a');
+		}
+	}
+
+	
+	if (ans == "") {
+		cout << "PREDAJA" << "\n";
+	}
+	else {
+		cout << ans << "\n";
+	}
+
+
+
+
+	return 0;
+}*/
