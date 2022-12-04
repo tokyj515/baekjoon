2097c685a7b15e658f624e8748f61e70d210040e
YuJin Kwon <tokyj515@naver.com>
2022-12-04 19:08:20 +0900

    22.12.04

---
+++ bj1233.cpp
@@ -0,0 +1,52 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int s1, s2, s3;
+int arr[81]; //ÃÖ´ë 80
+int maxVal = 0;
+
+int main() {
+	cin >> s1 >> s2 >> s3;
+
+
+	for (int i = 1; i <= s1; i++) {
+		for (int j = 1; j <= s2; j++) {
+			for (int k = 1; k <= s3; k++) {
+				arr[i+j+k]++;
+
+				maxVal = max({ maxVal, arr[i + j + k] });
+			}	
+		}
+	}
+
+	for (int i = 3; i <= s1 + s2 + s3; i++) {
+		if (maxVal == arr[i]) {
+			cout << i << "\n";
+			return 0;
+		}
+	}
+
+
+
+
+
+	return 0;
+}*/
