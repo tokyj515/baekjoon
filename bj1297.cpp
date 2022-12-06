d1cdf13ae8010cab3b2a7631369ed71cdb98409f
YuJin Kwon <tokyj515@naver.com>
2022-12-06 21:27:01 +0900

    22.12.06

---
+++ bj1297.cpp
@@ -0,0 +1,30 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+    double d, h, w;
+    cin >> d >> h >> w;
+
+    double temp = h * h + w * w;
+
+    cout << (int)sqrt((d * d * h * h / temp)) << ' ' << (int)sqrt((d * d * w * w / temp)) << '\n';
+
+}*/
