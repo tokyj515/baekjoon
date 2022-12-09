f8e9f0e2907352ef1b75d6db2fcfbe58e2d85b59
YuJin Kwon <tokyj515@naver.com>
2022-12-09 18:36:33 +0900

    22.12.09

---
+++ bj1392.cpp
@@ -0,0 +1,53 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int N, Q;
+int music[101];
+int question[1001];
+vector<int> res;
+
+int main() {
+	cin >> N >> Q;
+
+
+	for (int i= 0; i < N; i++) {
+		cin >> music[i];
+	}
+
+	for (int i = 0; i < Q; i++) {
+		cin >> question[i];
+	}
+
+	int cnt = 1;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			res.push_back(i);
+		}
+	}
+
+	for (int i = 0; i < Q; i++) {
+		cout << res[question[i]];
+	}
+
+
+	return 0;
+}*/
