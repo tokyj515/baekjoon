af91626eccb3bb66d2a1588fea154fb29d4e413c
YuJin Kwon <tokyj515@naver.com>
2022-12-12 22:36:05 +0900

    22.12.12

---
+++ bj26068.cpp
@@ -0,0 +1,42 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+string s;
+int cnt, t;
+
+int main() {
+	cin >> t;
+
+	while (t--) {
+		cin >> s;
+
+		s = s.erase(0, 2); //0부터 2개 지움
+
+		int time = stoi(s);
+
+		if (time <= 90) cnt++;
+	}
+
+	cout << cnt << "\n";
+
+
+	return 0;
+}*/
