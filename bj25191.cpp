1bc9c5058fe61fdc6d8cc74a22054f929b700bbb
YuJin Kwon <tokyj515@naver.com>
2022-12-14 18:49:36 +0900

    22.12.14

---
+++ bj25191.cpp
@@ -0,0 +1,35 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+/*
+int chicken;
+int coke, beer;
+int cnt = 0;
+
+int main() {
+	cin >> chicken >> coke >> beer;
+
+	cnt = coke / 2 + beer;
+
+
+	cout << min(cnt, chicken) << "\n";
+
+
+	return 0;
+}*/
