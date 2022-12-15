2b563c75a0d265a5d6faae299bba0aac150ff269
YuJin Kwon <tokyj515@naver.com>
2022-12-15 19:28:11 +0900

    22.12.15

---
+++ bj25192.cpp
@@ -0,0 +1,56 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+map<string, bool> m;
+int t;
+string s;
+int cnt = 0;
+
+
+
+int main() {
+	cin >> t;
+
+	while (t--) {
+		cin >> s;
+
+		if (s == "ENTER") {
+			m.clear();
+			continue;
+		}
+		
+		if (m.find(s) == m.end()) {
+			m.insert({ s, 1 });
+			cnt++;
+		}
+
+	
+	
+	
+	}
+
+	cout << cnt << "\n";
+	
+
+
+
+	return 0;
+}*/
