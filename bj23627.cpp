096f9a1e2943b2ead5c3b84ecb09c9aeac5e10f8
YuJin Kwon <tokyj515@naver.com>
2022-12-17 13:29:42 +0900

    22.12.17

---
+++ bj23627.cpp
@@ -0,0 +1,37 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+	string s, temp = "";
+	cin >> s;
+
+
+	for (int i = s.size() - 5; i <= s.size() - 1; i++) {
+		temp += s[i];
+	}
+
+	if (temp == "driip") cout << "cute" << "\n";
+	else cout << "not cute" << "\n";
+
+
+	return 0;
+}*/
