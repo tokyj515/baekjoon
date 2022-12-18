6c1cbd3ffb4ec5bca15d37922cba20d002aaa69e
YuJin Kwon <tokyj515@naver.com>
2022-12-18 17:35:34 +0900

    22.12.18

---
+++ bj26265.cpp
@@ -0,0 +1,55 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int t;
+vector<pair<string, string>>v;
+
+
+bool compare(pair<string, string>a, pair<string, string>b) {
+	if (a.first == b.first) {
+		return a.second > b.second;
+	}
+	else {
+		return a.first < b.first;
+	}
+
+}
+
+
+int main() {
+	cin >> t;
+
+	while (t--) {
+		string a, b;
+		cin >> a >> b;
+		v.push_back({ a, b });
+	}
+
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+
+
+	return 0;
+}*/
