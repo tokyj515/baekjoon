c3f47c6dce00cf75e1ddd11e0e69532200a9e726
YuJin Kwon <tokyj515@naver.com>
2022-12-22 13:20:04 +0900

    22.12.22

---
+++ bj11723.cpp
@@ -0,0 +1,67 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int s[21];
+
+int t;
+string op;
+int num;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	cin >> t;
+
+	while (t--) {
+		cin >> op >> num;
+
+		if (op == "add") {
+			if (!s[num]) s[num] = 1;
+		}
+		else if (op == "remove") {
+			if (s[num]) s[num] = 0;
+		}
+		else if (op == "check") {
+			if (s[num]) cout << "1" << "\n";
+			else cout << "0" << "\n";
+		}
+		else if (op == "toggle") {
+			if (s[num]) s[num] = 0;
+			else s[num] = 1;
+		}
+		else if (op == "all") {
+			for (int i = 1; i <= 20; i++) {
+				s[num] = 1;
+			}
+		}
+		else if (op == "empty") {
+
+			memset(s, 0, sizeof(s));
+		}
+	
+	
+	}
+
+
+	return 0;
+}*/
