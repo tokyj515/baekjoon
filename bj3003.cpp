141dde3c9c546e1a72fcd9dac0d2df8f10f040b5
YuJin Kwon <tokyj515@naver.com>
2022-12-23 18:58:53 +0900

    22.12.23

---
+++ bj3003.cpp
@@ -0,0 +1,32 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+int main() {
+    int input[6];
+    int chess[6] = { 1,1,2,2,2,8 };
+
+    for (int i = 0; i < 6; i++) {
+        cin >> input[i];
+        cout << chess[i] - input[i] << " ";
+    }
+    return 0;
+}*/
