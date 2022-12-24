b15a5df3e0a9ccc45fcc5d991416c2461a025b90
YuJin Kwon <tokyj515@naver.com>
2022-12-24 18:56:48 +0900

    22.12.24

---
+++ bj2563.cpp
@@ -0,0 +1,51 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int arr[101][101];
+int t;
+
+int main() {
+	cin >> t;
+
+	int area = 0;
+
+
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+
+
+		for (int i = a; i < a + 10; i++) {
+			for (int j = b; j < b + 10; j++) {
+				if (!arr[i][j]) {
+					area++;
+					arr[i][j] = 1;
+				}
+			}
+		}
+	}
+
+
+	cout << area << "\n";
+
+
+	return 0;
+}*/
