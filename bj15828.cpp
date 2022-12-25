2e227efa05762f688657c9f65da3502ee6cfe85e
YuJin Kwon <tokyj515@naver.com>
2022-12-25 20:27:57 +0900

    22.12.25

---
+++ bj15828.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+/*
+int main() {
+    int size;
+    cin >> size;
+    int num;
+    queue<int> q;
+
+    while (true)
+    {
+        cin >> num;
+        if (num == -1)
+            break;
+        if (num > 0)
+        {
+            if (q.size() < size)
+                q.push(num);
+        }
+        if (num == 0)
+            q.pop(); 
+    }
+    if (q.empty())
+        cout << "empty" << endl;
+    else
+        while (!q.empty())
+        {
+            cout << q.front() << ' ';
+            q.pop();
+        }
+}*/
