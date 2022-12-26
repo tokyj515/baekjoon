6cccc1154777ca0e9c3846ccc5c0cd52768c2f58
YuJin Kwon <tokyj515@naver.com>
2022-12-26 18:05:12 +0900

    22.12.26

---
+++ bj26517.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+/*
+int main() {
+	ll a, b, c, d, k;
+	cin >> k >> a >> b >> c >> d;
+
+
+	if (a * k + b == c * k + d) {
+		cout << "Yes " << a * k + b << "\n";
+	}
+	else {
+		cout << "No" << "\n";
+	}
+
+
+	return 0;
+}*/
