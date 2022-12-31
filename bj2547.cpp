f970d24ecbc32810c26a903b6ea1afa5788874f9
YuJin Kwon <tokyj515@naver.com>
2022-12-31 18:31:16 +0900

    22.12.31

---
+++ bj2547.cpp
@@ -0,0 +1,37 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+typedef pair<int, int> PII;
+
+
+/*
+
+int t;
+int main() {
+    cin >> t;
+    while (t--) {
+        ll student, totalCandy = 0, candy;
+        cin >> student;
+        for (int i = 0; i < student; i++) {
+            cin >> candy;
+            totalCandy = (totalCandy + candy) % student;
+        }
+        if (totalCandy % student) cout << "NO\n";
+        else cout << "YES\n";
+    }
+}*/
