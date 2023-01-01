57b41b47b70e541ca6c8ec997c11827903cbae9e
YuJin Kwon <tokyj515@naver.com>
2023-01-01 15:06:37 +0900

    23.01.01

---
+++ bj2765.cpp
@@ -0,0 +1,35 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+using namespace std;
+
+
+/*
+int cnt;
+int main() {
+    while (1) {
+        cnt++;
+        double diameter, rotated, sec;
+        cin >> diameter >> rotated >> sec;
+        if (!rotated) break;
+        double moved = diameter / (5280.0 * 12.0) * rotated * pi;
+        printf("Trip #%d: %.2f %.2f\n", cnt, moved, moved / sec * 3600);
+    }
+}*/
