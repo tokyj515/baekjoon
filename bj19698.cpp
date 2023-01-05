0ad6fefb661434a29216289df16bc62a7005d96e
YuJin Kwon <tokyj515@naver.com>
2023-01-05 19:12:35 +0900

    23.01.05

---
+++ bj19698.cpp
@@ -0,0 +1,35 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+using namespace std;
+
+
+/*
+int n, w, h, l;
+
+int main() {
+	cin >> n >> w >> h >> l;
+
+	cout << min((w/l) * (h/l), n);
+
+
+
+	return 0;
+}*/
