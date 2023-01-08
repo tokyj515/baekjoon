1e6681428169c0bc5cf50e8e5a60f71902cc5055
YuJin Kwon <tokyj515@naver.com>
2023-01-08 13:28:24 +0900

    23.01.08

---
+++ bj15680.cpp
@@ -0,0 +1,38 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+using namespace std;
+
+
+/*
+int main() {
+	int n;
+	cin >> n;
+
+	if (n == 0) {
+		cout << "YONSEI";
+	}
+	else if(n==1) {
+		cout << "Leading the Way to the Future";
+	}
+
+
+	return 0;
+}*/
