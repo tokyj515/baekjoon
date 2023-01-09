1513cf6c2131b5df5f46882ac0796cef0300694e
YuJin Kwon <tokyj515@naver.com>
2023-01-09 21:42:52 +0900

    23.01.09

---
+++ bj17356.cpp
@@ -0,0 +1,37 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+using namespace std;
+
+
+/*
+int a, b;
+
+int main() {
+	cin >> a >> b;
+
+	double m = (b - a) / 400;
+
+
+	printf("%.20f", 1 / (1 + pow(10, m)));
+
+
+	return 0;
+}*/
