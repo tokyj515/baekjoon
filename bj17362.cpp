13c275b0014e6902d1fb0ce30af711290ed45e6d
YuJin Kwon <tokyj515@naver.com>
2023-01-10 18:59:16 +0900

    23.01.10

---
+++ bj17362.cpp
@@ -0,0 +1,46 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+using namespace std;
+
+/*
+
+int main() {
+	int n;
+	cin >> n;
+
+	n %= 8;
+
+	if (n == 1)
+		cout << 1 << "\n";
+	else if (n == 2 || n == 0)
+		cout << 2 << "\n";
+	else if (n == 3 || n == 7)
+		cout << 3 << "\n";
+	else if (n == 4 || n == 6)
+		cout << 4 << "\n";
+	else if (n == 5)
+		cout << 5 << "\n";
+
+
+
+
+	return 0;
+}*/
