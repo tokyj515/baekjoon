10f5159d7ddd008070e68fdbfb0bf1c84bc261e2
YuJin Kwon <tokyj515@naver.com>
2023-01-12 19:10:33 +0900

    23.01.12

---
+++ bj1312.cpp
@@ -0,0 +1,31 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+
+/*
+int a, b, n, ans;
+int main() {
+    cin >> a >> b >> n;
+    for (int i = 0; i < n + 1; i++) {
+        ans = a / b;
+        a = a % b * 10;
+    }
+    cout << ans;
+}*/
