478dc2077e1c526ce84668a3dfd7d217edf51c17
YuJin Kwon <tokyj515@naver.com>
2023-01-14 16:19:50 +0900

    23.01.14

---
+++ bj9653.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable : 4996)
+using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cmath>
+#include <map>
+#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define ll long long
+#define pi 3.14159265359
+typedef pair<int, int> PII;
+
+/*
+int main() {
+    printf("    8888888888  888    88888\n");
+    printf("   88     88   88 88   88  88\n");
+    printf("    8888  88  88   88  88888\n");
+    printf("       88 88 888888888 88   88\n");
+    printf("88888888  88 88     88 88    888888\n");
+    printf("\n");
+    printf("88  88  88   888    88888    888888\n");
+    printf("88  88  88  88 88   88  88  88\n");
+    printf("88 8888 88 88   88  88888    8888\n");
+    printf(" 888  888 888888888 88  88      88\n");
+    printf("  88  88  88     88 88   88888888\n");
+    return 0;
+}*/
